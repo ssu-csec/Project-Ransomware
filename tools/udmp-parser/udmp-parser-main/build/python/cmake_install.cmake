cc44242849a1d200aa10914fb7c70f5dfe7942fb
kdw5351 <160556627+kdw5351@users.noreply.github.com>
2025-11-04 10:46:03 +0900

    feat: add udmp-parser under tools/

---
+++ tools/udmp-parser/udmp-parser-main/build/python/cmake_install.cmake
@@ -0,0 +1,76 @@
+# Install script for directory: C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/src/python
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/udmp-parser")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bindings/python" TYPE MODULE FILES "C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/build/python/Debug/udmp_parser.cp313-win_amd64.pyd")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bindings/python" TYPE MODULE FILES "C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/build/python/Release/udmp_parser.cp313-win_amd64.pyd")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bindings/python" TYPE MODULE FILES "C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/build/python/MinSizeRel/udmp_parser.cp313-win_amd64.pyd")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bindings/python" TYPE MODULE FILES "C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/build/python/RelWithDebInfo/udmp_parser.cp313-win_amd64.pyd")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    include("C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/build/python/CMakeFiles/udmp_parser.dir/install-cxx-module-bmi-Debug.cmake" OPTIONAL)
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    include("C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/build/python/CMakeFiles/udmp_parser.dir/install-cxx-module-bmi-Release.cmake" OPTIONAL)
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    include("C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/build/python/CMakeFiles/udmp_parser.dir/install-cxx-module-bmi-MinSizeRel.cmake" OPTIONAL)
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    include("C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/build/python/CMakeFiles/udmp_parser.dir/install-cxx-module-bmi-RelWithDebInfo.cmake" OPTIONAL)
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bindings/python" TYPE FILE OPTIONAL FILES "C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/build/python/Debug/udmp_parser.pdb")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bindings/python" TYPE FILE OPTIONAL FILES "C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/build/python/Release/udmp_parser.pdb")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bindings/python" TYPE FILE OPTIONAL FILES "C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/build/python/MinSizeRel/udmp_parser.pdb")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bindings/python" TYPE FILE OPTIONAL FILES "C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/build/python/RelWithDebInfo/udmp_parser.pdb")
+  endif()
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "C:/Users/user/Desktop/udmp-parser-main/udmp-parser-main/build/python/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
